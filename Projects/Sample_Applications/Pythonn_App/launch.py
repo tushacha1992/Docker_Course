b6ef3f24fe3849698e1cd121e2b65925c3cd9416
tushacha1992 <45059860+tushacha1992@users.noreply.github.com>
2021-04-04 11:58:09 +0530

    Update launch.py

--- Projects/Sample_Applications/Pythonn_App/launch.py
+++ Projects/Sample_Applications/Pythonn_App/launch.py
@@ -1,7 +1,7 @@
 from flask import Flask
-helloworld = Flask(__name__)
+WelcomePython = Flask(__name__)
 @helloworld.route("/")
 def run():
     return "{\"message\":\"Welcome to Python Application V1\"}"
 if __name__ == "__main__":
-    helloworld.run(host="0.0.0.0", port=int("5000"), debug=True)
+    WelcomePython.run(host="0.0.0.0", port=int("5000"), debug=True)
